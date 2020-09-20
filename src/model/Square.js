024220acece5032ac931d9cfdbac1c9ac41a1a35
ntp28459hteam <ntp28459hteam@gmail.com>
2020-09-20 09:30:32 +0700

    init project

---
+++ src/model/Square.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button 
+        className="square" 
+        onClick={() => {this.props.onClick()}}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+export default Square;
